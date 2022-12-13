cd4b38e5481894673359c49fc2c68b60821dacd1
“Anton <antonprokapnev@gmail.com>
2022-12-14 01:55:06 +0300

    Remove null check in MeetupRepository

--- MeetupWebApi/MeetupWebApi.DAL/Exceptions/DbContextNullException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MeetupWebApi.DAL.Exceptions
-{
-    public class DbContextNullException:Exception
-    {
-        public DbContextNullException(string message):base(message) { }
-    }
-}
--- MeetupWebApi/MeetupWebApi.DAL/Repositories/MeetupRepository.cs
+++ MeetupWebApi/MeetupWebApi.DAL/Repositories/MeetupRepository.cs
@@ -1,25 +1,15 @@
 ﻿using MeetupWebApi.DAL.EF;
-using MeetupWebApi.DAL.Exceptions;
 using MeetupWebApi.DAL.Interfaces;
 using MeetupWebApi.DAL.Models;
-using Microsoft.Extensions.Logging;
 
 namespace MeetupWebApi.DAL.Repositories
 {
     public class MeetupRepository:GenericRepository<Meetup>,IMeetupRepository
     {
         private readonly AppDbContext _context;
-        private readonly ILogger<MeetupRepository> _logger;
 
-        public MeetupRepository(AppDbContext context,ILogger<MeetupRepository> logger) : base(context)
-        {
-            _logger = logger;
-            if (context== null)
-            {
-                string errorMessage = "Db context is null";
-                _logger.LogError(errorMessage);
-                throw new DbContextNullException(errorMessage);
-            }
+        public MeetupRepository(AppDbContext context) : base(context)
+        {            
             _context = context;            
         }
     }
