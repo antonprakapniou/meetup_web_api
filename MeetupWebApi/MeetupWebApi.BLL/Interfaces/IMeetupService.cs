57ab8450335e4bf6067afeef0c8d6ee686693607
“Anton <antonprokapnev@gmail.com>
2022-12-14 02:46:27 +0300

    Create IMeetupService with a set CRUD-operations

---
+++ MeetupWebApi/MeetupWebApi.BLL/Interfaces/IMeetupService.cs
@@ -0,0 +1,17 @@
+﻿using MeetupWebApi.BLL.DTO;
+
+namespace MeetupWebApi.BLL.Interfaces
+{
+    public interface IMeetupService
+    {
+        public Task<IEnumerable<MeetupDto>> GetMeetupsAsync();
+
+        public Task<MeetupDto> GetMeetupByIdAsync(int id);
+
+        public Task<MeetupDto> CreateMeetupAsync(MeetupDto meetupDto);
+
+        public Task<MeetupDto> UpdateMeetupAsync(MeetupDto meetupDto);
+
+        public Task<MeetupDto> DeleteMeetupAsync(MeetupDto meetupDto);
+    }
+}
