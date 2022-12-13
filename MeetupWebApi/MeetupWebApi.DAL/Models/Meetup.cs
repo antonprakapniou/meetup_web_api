1152b51b711e38e81fddcf857138a5220cfe7719
“Anton <antonprokapnev@gmail.com>
2022-12-13 20:11:39 +0300

    Create Meetup model

---
+++ MeetupWebApi/MeetupWebApi.DAL/Models/Meetup.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetupWebApi.DAL.Models
+{
+    public class Meetup
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Topic { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        public string? Schedule { get; set; }
+
+        [Required]
+        public string? Sponsor { get; set; }
+
+        [Required]
+        public string? Speaker { get; set; }
+
+        [Required]
+        public string? Adress { get; set; }
+
+        //use Utc for work with PostgreSQL 
+        [Required]
+        public DateTime? Spending { get; set; }
+    }
+}
