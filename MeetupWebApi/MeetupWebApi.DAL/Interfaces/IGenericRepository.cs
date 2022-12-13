c18e1fb162ee5ad7928808615ac83ba2e2a71057
“Anton <antonprokapnev@gmail.com>
2022-12-14 00:55:54 +0300

    Create IGenericRepository with a set CRUD-operations

---
+++ MeetupWebApi/MeetupWebApi.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace MeetupWebApi.DAL.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        public Task AddAsync(T entity);
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<IEnumerable<T>> GetByAsync(Expression<Func<T, bool>> expression);
+        public void Update(T entity);
+        public void Delete(T entity);
+        public Task SaveChangesAsync();
+    }
+}
